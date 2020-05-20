6684e31dd4baf11d7abaf580078eef23328ce022
antonkushch <a.kushch@levi9.com>
2020-05-20 10:56:20 +0300

    Add intro scene

---
+++ Assets/_APP/Scripts/IntroGoToText.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AssemblyCSharp
+{
+	public class IntroGoToText : MonoBehaviour
+	{
+		public GameObject current;
+
+		void Start()
+		{
+			
+			//scaleAnimation.GetComponents<Animator>();
+		}
+
+		public void StartScaleGoHere()
+		{
+			for (int i = 0; i < current.transform.childCount; i++)
+			{
+				var child = current.transform.GetChild(i).gameObject;
+				var anim = child.GetComponent<Animator> ();
+				if (anim == null)
+					continue;
+				anim.Play("GoHereAnimation");
+			}
+			//scaleAnimation.Play("GoHereAnimation");
+		}
+		public void StopScaleGoHere()
+		{
+			//scaleAnimation.Play("none");
+			for (int i = 0; i < current.transform.childCount; i++)
+			{
+				var child = current.transform.GetChild(i).gameObject;
+				var anim = child.GetComponent<Animator> ();
+				if (anim == null)
+					continue;
+				anim.Play("none");
+			}
+		}
+	}
+}
+
